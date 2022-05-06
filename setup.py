2a87486891bf4558dea46cc6c176547299a27132
itay62848 <itay62848@gmail.com>
2022-05-06 23:17:01 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,56 +1,58 @@
-from setuptools import setup
 import os
-import io
+import re
+from setuptools import setup, find_packages
 
-workdir = os.path.abspath(os.path.dirname(__file__))
-
-# https://packaging.python.org/single_source_version/
-with open(os.path.join(workdir, "scapy_p0f", "__init__.py")) as fp:
-    __version__ = fp.read().split('__version__ = "', 1)[1].split('"', 1)[0]
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_long_description():
-    """Extract description from README.md, for PyPI's usage"""
-    try:
-        fpath = os.path.join(os.path.dirname(__file__), "README.md")
-        with io.open(fpath, encoding="utf-8") as f:
-            readme = f.read()
-            return readme
-    except IOError:
-        return None
+def read(*parts):
+    with open(os.path.join(HERE, *parts), "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def get_version():
+    """
+    Return version as listed in `__version__` in `__init__.py`.
+    """
+    init_py = read(os.path.join("pyp0f", "__init__.py"))
+    match = re.search(r"__version__ = ['\"]([^'\"]*)['\"]", init_py)
+    assert match
+    return match.group(1)
 
 
 setup(
-    name="scapy-p0f",
-    version=__version__,
-    description="p0f v3 clone written in Python",
-    long_description=get_long_description(),
+    name="pyp0f",
+    version=get_version(),
+    description="p0f (v3) written in Python",
+    long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    url="https://github.com/Nisitay/scapy-p0f",
-    download_url="https://github.com/Nisitay/scapy-p0f/tarball/master",
+    url="https://github.com/Nisitay/pyp0f",
+    keywords=["fingerprint", "p0f", "scapy", "network", "packets"],
     author="Itay Margolin",
     author_email="itay62848@gmail.com",
     license="MIT",
-    keywords=["p0f", "scapy", "network", "packets"],
-    packages=["scapy_p0f"],
-    package_data={"scapy_p0f": ["data/p0f.fp"]},
+    packages=find_packages(include=("pyp0f", "pyp0f.*")),
     include_package_data=True,
-    install_requires=["scapy"],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
+    python_requires=">=3.7",
+    install_requires=["scapy>=2.4.5"],
     classifiers=[
-        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Security",
-        "Topic :: System :: Networking",
-        "Topic :: Software Development :: Libraries :: Python Modules"
+        "Topic :: Internet",
+        "Topic :: System :: Networking :: Monitoring",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed"
     ],
 )
