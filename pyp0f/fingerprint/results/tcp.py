a33ebfe7f69030d11d235fd5ae2d89a884c097f3
itay62848 <itay62848@gmail.com>
2023-09-28 19:12:54 +0300

    Fix doc string

--- pyp0f/fingerprint/results/tcp.py
+++ pyp0f/fingerprint/results/tcp.py
@@ -29,7 +29,7 @@ class TCPMatch:
 
     @property
     def is_fuzzy(self) -> bool:
-        """approximate match?"""
+        """Approximate match?"""
         return self.type != TCPMatchType.EXACT
 
 
